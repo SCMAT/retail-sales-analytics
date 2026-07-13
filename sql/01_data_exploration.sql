0a50936d510bfa6ac6ab50dcd4e061074d0ddff9
Seboye Matlou <83583267+SCMAT@users.noreply.github.com>
2026-07-13 20:45:43 +0200

    Update 01_data_exploration.sql

--- sql/01_data_exploration.sql
+++ sql/01_data_exploration.sql
@@ -15,3 +15,53 @@ SELECT SUM(TRY_CONVERT(decimal(18,2), Quantity)) AS Total_Quantity
 FROM superstore;
 SELECT DISTINCT Region
 FROM superstore;
+
+SELECT
+    Region,
+    ROUND(SUM(Sales), 2) AS Total_Sales
+FROM superstore
+GROUP BY Region
+ORDER BY Total_Sales DESC;
+SELECT
+    Region,
+    ROUND(SUM(Profit), 2) AS Total_Profit
+FROM superstore
+GROUP BY Region
+ORDER BY Total_Profit DESC;
+SELECT
+    Category,
+    ROUND(SUM(Sales), 2) AS Total_Sales
+FROM superstore
+GROUP BY Category
+ORDER BY Total_Sales DESC;
+SELECT
+    Category,
+    ROUND(SUM(Profit), 2) AS Total_Profit
+FROM superstore
+GROUP BY Category
+ORDER BY Total_Profit DESC;
+SELECT TOP 10
+    Sub_Category,
+    ROUND(SUM(Sales), 2) AS Total_Sales
+FROM superstore
+GROUP BY Sub_Category
+ORDER BY Total_Sales DESC;
+SELECT TOP 10
+    Sub_Category,
+    ROUND(SUM(Profit), 2) AS Total_Profit
+FROM superstore
+GROUP BY Sub_Category
+ORDER BY Total_Profit ASC;
+SELECT
+    Discount,
+    ROUND(AVG(Profit),2) AS Avg_Profit
+FROM superstore
+GROUP BY Discount
+ORDER BY Discount;
+SELECT
+    Segment,
+    ROUND(SUM(Sales),2) AS Total_Sales,
+    ROUND(SUM(Profit),2) AS Total_Profit
+FROM superstore
+GROUP BY Segment
+ORDER BY Total_Sales DESC;
