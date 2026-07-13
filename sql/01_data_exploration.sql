9bdb97ee1afd4e93bf62fc9a36501db91649eb66
Seboye Matlou <83583267+SCMAT@users.noreply.github.com>
2026-07-13 20:41:31 +0200

    Update 01_data_exploration.sql
    Initial SQL changes

--- sql/01_data_exploration.sql
+++ sql/01_data_exploration.sql
@@ -1,1 +1,17 @@
-
+SELECT *
+FROM superstore;
+SELECT COUNT(*) AS Total_Records
+FROM superstore;
+SELECT COUNT(*)
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'superstore';
+SELECT SUM(Sales) AS Total_Sales
+FROM superstore;
+SELECT SUM(TRY_CONVERT(decimal(18,2), Sales)) AS Total_Sales
+FROM dbo.Superstore;
+SELECT SUM(TRY_CONVERT(decimal(18,2), Profit)) AS Total_Profit
+FROM superstore;
+SELECT SUM(TRY_CONVERT(decimal(18,2), Quantity)) AS Total_Quantity
+FROM superstore;
+SELECT DISTINCT Region
+FROM superstore;
